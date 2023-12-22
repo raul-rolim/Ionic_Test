8a9e62edf8baf9d0de858bbc494ceecd00c9adac
Raul de Luca <rauldeveloperproject1447@gmail.com>
2023-12-21 21:07:36 -0300

    Register Page Finished

--- ionic_test/src/app/app-routing.module.ts
+++ ionic_test/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'loader',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
-  },  {
+  },
+  {
     path: 'register',
     loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
   }
